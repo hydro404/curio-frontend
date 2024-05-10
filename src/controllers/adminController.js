eb19cca00f7e15fc25f749c06fc94b1fea4b7333
hydro404 <luisanthonynuyda.grado@bicol-u.edu.ph>
2024-05-10 23:34:21 +0800

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,39 +1,37 @@
-const axios = require('axios');
+const axios = require("axios");
 //dotenv
-require('dotenv').config();
+require("dotenv").config();
 
 var SERVER_URL = process.env.SERVER_URL;
 
 const addProduct = async (req, res) => {
-    const cookieData = req.cookies.token;
-    const { name, price, category, description, image } = req.body;
-    if (!name || !price || !category || !description || !image) {
-        return res.status(400).json({
-            message: 'Name, price, category, description, and image are required.',
-            status: 'error',
-        }
-            
-        );
-    }
-
-    try {
-        const response = await axios.post(`${SERVER_URL}/admin/products`, { name, price, category, description, image }, 
-        {
-            headers: { Authorization: `${cookieData}` },
-        }
-        );
-        if (response.data.status === "success") {
-            res.status(200).json({
-                message: 'Product added',
-                status: 'success',
-            });
-        } else {
-            res.status(400).send('Something went wrong');
-        }
-    } catch (error) {
-        console.error('Add product error:', error);
-        res.status(500).send('Server error during add product');
-    }
-}
+  const cookieData = req.cookies.token;
+  console.log(cookieData);
+  const { name, description, price, category, stock } = req.body;
+  const images = req.files.map((file) => file.filename);
+  console.log(images);
+  axios
+    .post(
+      `${SERVER_URL}/admin/products`,
+      {
+        name: name,
+        description: description,
+        price: price,
+        category: category,
+        stock: stock,
+        images: images,
+      },
+      {
+        headers: { authorization: `${cookieData}` },
+      }
+    )
+    .then((response) => {
+      res.redirect("/admin-update");
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Internal Server Error");
+    });
+};
 
 module.exports = { addProduct };
